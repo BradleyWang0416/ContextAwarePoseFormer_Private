aea4c1330a7c543c772a3b9d925abdec6b76f560
BradleyWang <993369949@qq.com>
2025-07-30 16:50:41 +0000

    preprocess_3dpw_byBradley.py Done. saved to /data2/wxs/DATASETS/PW3D_ByBradley/

--- H36M-Toolbox/preprocess_3dpw_byBradley.py
+++ H36M-Toolbox/preprocess_3dpw_byBradley.py
@@ -31,18 +31,41 @@ ALL_DATA = {
     'train': {
         'joint_2d': [],
         'joint3d_image': [],
-        'joints_2.5d_image': [],
+        'joint3d_cam': [],
+        'joint3d_world': [],
+        # 'joints_2.5d_image': [],
         '2.5d_factor': [],
         'source': [],
         'res': [],
+        'campose_valid': [],
         },
     'test': {
         'joint_2d': [],
         'joint3d_image': [],
-        'joints_2.5d_image': [],
+        'joint3d_cam': [],
+        'joint3d_world': [],
+        # 'joints_2.5d_image': [],
         '2.5d_factor': [],
         'source': [],
         'res': [],
+        'campose_valid': [],
+        },
+}
+
+ALL_SMPL_DATA = {
+    'train': {
+        'poses': [],
+        'trans': [],
+        'betas': [],
+        'gender': [],
+        'source': [],
+        },
+    'test': {
+        'poses': [],
+        'trans': [],
+        'betas': [],
+        'gender': [],
+        'source': [],
         },
 }
 
@@ -57,6 +80,31 @@ def main():
             #            'cam_intrinsics', 'cam_poses', 'campose_valid', 
             #            'betas', 'betas_clothed', 'v_template_clothed', 
             #            'genders', 'texture_maps'])
+            """
+            The 3DPW dataset contains several motion sequences, which are organized into two folders: imageFiles and sequenceFiles.
+            The folder imageFiles contains the RGB-images for every sequence. 
+            The folder sequenceFiles provides synchronized motion data and SMPL model parameters in the form of .pkl-files. For each sequence, the .pkl-file contains a dictionary with the following fields:
+            - sequence: String containing the sequence name
+            - betas: SMPL shape parameters for each actor which has been used for tracking (List of 10x1 SMPL beta parameters)
+            - poses: SMPL body poses for each actor aligned with image data (List of Nx72 SMPL joint angles, N = #frames)
+            - trans: tranlations for each actor aligned with image data (List of Nx3 root translations)
+            - poses_60Hz: SMPL body poses for each actor at 60Hz (List of Nx72 SMPL joint angles, N = #frames)
+            - trans_60Hz: tranlations for each actor at 60Hz (List of Nx3 root translations)
+            - betas_clothed: SMPL shape parameters for each clothed actor (List of 10x1 SMPL beta parameters)
+            - v_template_clothed: 
+            - gender: actor genders (List of strings, either 'm' or 'f')
+            - texture_maps: texture maps for each actor
+            - poses2D: 2D joint detections in Coco-Format for each actor (only provided if at least 6 joints were detected correctly)
+            - jointPositions: 3D joint positions of each actor (List of Nx(24*3) XYZ coordinates of each SMPL joint)
+            - img_frame_ids: an index-array to down-sample 60 Hz 3D poses to corresponding image frame ids
+            - cam_poses: camera extrinsics for each image frame (Ix4x4 array, I frames times 4x4 homegenous rigid body motion matrices)
+            - campose_valid: a boolean index array indicating which camera pose has been aligned to the image
+            - cam_intrinsics: camera intrinsics (K = [f_x 0 c_x;0 f_y c_y; 0 0 1])
+
+            Each sequence has either one or two models, which corresponds to the list size of the model specific fields (e.g. betas, poses, trans, v_template, gender, texture_maps, jointPositions, poses2D). 
+            SMPL poses and translations are provided at 30 Hz. They are aligned to image dependent data (e.g. 2D poses, camera poses). In addition we provide 'poses_60Hz' and 'trans_60Hz' which corresponds to the recording frequency of 60Hz of the IMUs . You could use the 'img_frame_ids' to downsample and align 60Hz 3D and image dependent data, wich has been done to compute SMPL 'poses' and 'trans' variables. 
+            Please refer to the demo.py-file for loading a sequence, setup smpl-Models and camera, and to visualize an example frame.
+            """
             image_folder = os.path.join(THREEDPW_IMAGE, data['sequence'])    # e.g., '/data2/wxs/DATASETS/PW3D/imageFiles/downtown_car_00'
             img_path = os.path.join(image_folder, f"image_{data['img_frame_ids'][0]:05d}.jpg")  # 读取第 1 帧图片
             img = cv2.imread(img_path)
@@ -103,8 +151,24 @@ def main():
                 joint_2d_Yflip = joint_2d.copy()
                 joint_2d_Yflip[..., 1] = res_h - joint_2d_Yflip[..., 1]  # Y轴翻转, 单位: 像素
 
-                
 
+                source = data['sequence'] + str(p_id)
+
+                ALL_DATA[split_name]['joint_2d'].append(joint_2d)
+                ALL_DATA[split_name]['joint3d_image'].append(joint_3d_image)
+                ALL_DATA[split_name]['joint3d_cam'].append(joint_3d_cam)
+                ALL_DATA[split_name]['joint3d_world'].append(joint_3d_world)
+                # ALL_DATA[split_name]['joints_2.5d_image'].append()
+                ALL_DATA[split_name]['2.5d_factor'].append(factor_2_5d)
+                ALL_DATA[split_name]['source'] = ALL_DATA[split_name]['source'] + [source] * joint_2d.shape[0]
+                ALL_DATA[split_name]['res'].append(np.array([[res_w, res_h]]).repeat(joint_2d.shape[0], 0))
+                ALL_DATA[split_name]['campose_valid'].append(data['campose_valid'][p_id])
+
+                ALL_SMPL_DATA[split_name]['poses'].append(data['poses'][p_id])
+                ALL_SMPL_DATA[split_name]['trans'].append(data['betas'][p_id][None, :10].repeat(joint_2d.shape[0], 0))
+                ALL_SMPL_DATA[split_name]['betas'].append(data['trans'][p_id])
+                ALL_SMPL_DATA[split_name]['gender'] = ALL_SMPL_DATA[split_name]['gender'] + [gender] * joint_2d.shape[0]
+                ALL_SMPL_DATA[split_name]['source'] = ALL_SMPL_DATA[split_name]['source'] + [source] * joint_2d.shape[0]
 
                 # TODO. 利用 3DPW 数据集提供的 demo.py 渲染第1帧姿态
                 """
@@ -123,6 +187,19 @@ def main():
                 w = int(2*cam.c[0])
                 """
 
+        ALL_DATA[split_name]['joint_2d'] = np.concatenate(ALL_DATA[split_name]['joint_2d'], axis=0)  # (T,17,2)
+        ALL_DATA[split_name]['joint3d_image'] = np.concatenate(ALL_DATA[split_name]['joint3d_image'], axis=0)  # (T,17,3)
+        ALL_DATA[split_name]['joint3d_cam'] = np.concatenate(ALL_DATA[split_name]['joint3d_cam'], axis=0)  # (T,17,3)
+        ALL_DATA[split_name]['joint3d_world'] = np.concatenate(ALL_DATA[split_name]['joint3d_world'], axis=0)  # (T,17,3)
+        ALL_DATA[split_name]['2.5d_factor'] = np.concatenate(ALL_DATA[split_name]['2.5d_factor'], axis=0)  # (T,)
+        ALL_DATA[split_name]['res'] = np.concatenate(ALL_DATA[split_name]['res'], axis=0)  # (T,)
+        ALL_DATA[split_name]['campose_valid'] = np.concatenate(ALL_DATA[split_name]['campose_valid'], axis=0)  # (T,)
+
+        ALL_SMPL_DATA[split_name]['poses'] = np.concatenate(ALL_SMPL_DATA[split_name]['poses'], axis=0)  # (T,72)
+        ALL_SMPL_DATA[split_name]['trans'] = np.concatenate(ALL_SMPL_DATA[split_name]['trans'], axis=0)  # (T,10)
+        ALL_SMPL_DATA[split_name]['betas'] = np.concatenate(ALL_SMPL_DATA[split_name]['betas'], axis=0)  # (T,10)
+
+    return 
 
 def _infer_box(pose3d, fx, fy, cx, cy, rootIdx=0):
     root_joint = pose3d[..., rootIdx, :]
